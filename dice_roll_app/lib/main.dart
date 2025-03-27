ebfc3610e993d57b9a480168a99e932209c3e795
JacquesBe <jacques.becker@paytec.global>
2025-03-27 16:15:36 +0200

    const

--- dice_roll_app/lib/main.dart
+++ dice_roll_app/lib/main.dart
@@ -2,5 +2,5 @@ import 'package:dice_roll_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
 }
